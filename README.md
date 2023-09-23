a2de3a051ff22cda2fb523bb9f513843ad280f79
Pebaz <pebazium@gmail.com>
2023-09-23 11:18:53 -0700

    Add example to readme

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # janet-rs-sys
+
 Raw Rust bindings to the Janet Programming Language
+
+```rust
+use janet_rs_sys::*;
+use std::ffi::*;
+
+fn main()
+{
+    let result = unsafe { janet_init() };
+    assert_eq!(result, 0);
+
+    let env = unsafe { janet_core_env(std::ptr::null_mut()) };
+
+    let code =
+        CString::new(r#"(print "Hello World!")"#).unwrap().as_c_str().as_ptr();
+
+    let file = CString::new("hello-world.janet").unwrap().as_c_str().as_ptr();
+
+    let result =
+        unsafe { janet_dostring(env, code, file, std::ptr::null_mut()) };
+    assert_eq!(result, 0);
+}
+```
--- src/lib.rs
+++ src/lib.rs
@@ -5,25 +5,29 @@
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 #[cfg(test)]
-mod tests {
+mod tests
+{
     use super::*;
     use std::ffi::*;
 
     #[test]
-    fn test_janet_bindings() {
+    fn test_janet_bindings()
+    {
         let result = unsafe { janet_init() };
         assert_eq!(result, 0);
 
         let env = unsafe { janet_core_env(std::ptr::null_mut()) };
+
         let code = CString::new(r#"(print "Hello World!")"#)
             .unwrap()
             .as_c_str()
             .as_ptr();
-        let file = CString::new("hello-world.janet")
-            .unwrap()
-            .as_c_str()
-            .as_ptr();
-        let result = unsafe { janet_dostring(env, code, file, std::ptr::null_mut()) };
+
+        let file =
+            CString::new("hello-world.janet").unwrap().as_c_str().as_ptr();
+
+        let result =
+            unsafe { janet_dostring(env, code, file, std::ptr::null_mut()) };
         assert_eq!(result, 0);
     }
 }
